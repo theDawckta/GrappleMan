492cd9902f1b0bf25a975010074c7aa66c20750a
Zeke Lasater <zekelasater@Zekes-MacBook-Pro.local>
2018-10-04 23:55:12 -0700

    modifying Player property settings

---
+++ Assets/Scripts/PlaygroundSceneController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Grappler;
+using Grappler.Data;
+using Grappler.Util;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaygroundSceneController : MonoBehaviour
+{
+    public PlayerController Player;
+
+    private AudioSource _playerAudio;
+    private AudioClip _song;
+    private Camera _mainCamera;
+    private Vector3 _mainCameraStartPosition;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+        _playerAudio = GetComponent<AudioSource>();
+        _song = Resources.Load("Songs/BeatOfTheTerror") as AudioClip;
+        _playerAudio.clip = _song;
+        _playerAudio.loop = true;
+        _mainCamera = Camera.main;
+        _mainCameraStartPosition = _mainCamera.transform.position;
+    }
+
+    void Start()
+    {
+        Player.Init();
+    }
+
+    void OnEnable()
+	{
+    }
+
+	void OnDisable()
+	{
+    }
+}
