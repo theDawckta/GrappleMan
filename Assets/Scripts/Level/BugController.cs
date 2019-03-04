98e1be026e9f4dc8066f6e47a3b47e66a257c384
theDawckta <lasaterz@gmail.com>
2019-03-04 10:59:32 -0800

    bug walking working better

--- Assets/Scripts/Level/BugController.cs
+++ Assets/Scripts/Level/BugController.cs
@@ -19,36 +19,59 @@ public class BugController : MonoBehaviour
     private RaycastHit _hit;
     private Vector3 _direction;
     private float _maxDistance;
+    //private float _blPreviousDistance;
+    //private float _brPreviousDistance;
+    private float _frPreviousDistance;
+    private float _flPreviousDistance;
 
     void Start ()
     {
        _maxDistance = Vector3.Distance(FRRaycastOrigin.position, Hand.position);
+        _flPreviousDistance = _maxDistance;
     }
 	
 	void Update ()
     {
-        Debug.Log(Vector3.Distance(BRRaycastOrigin.position, BRTarget.position));
-        if (Vector3.Distance(BRRaycastOrigin.position, BRTarget.position) > 35.0f && !DOTween.IsTweening(BRTarget, true))
-            PlaceLeg(-1, BRTarget, BRRaycastOrigin);
-        if (Vector3.Distance(BLRaycastOrigin.position, BLTarget.position) > 35.0f && !DOTween.IsTweening(BLTarget, true))
-            PlaceLeg(1, BLTarget, BLRaycastOrigin);
-        if (Vector3.Distance(FRRaycastOrigin.position, FRTarget.position) > 35.0f && !DOTween.IsTweening(FRTarget, true))
-            PlaceLeg(-1, FRTarget, FRRaycastOrigin);
-        if (Vector3.Distance(FLRaycastOrigin.position, FLTarget.position) > 35.0f && !DOTween.IsTweening(FLTarget, true))
+        //if (Vector3.Distance(BRRaycastOrigin.position, BRTarget.position) > 35.0f && !DOTween.IsTweening(BRTarget, true))
+        //    PlaceLeg(80, BRTarget, BRRaycastOrigin);
+        //if (Vector3.Distance(BLRaycastOrigin.position, BLTarget.position) > 35.0f && !DOTween.IsTweening(BLTarget, true))
+        //    PlaceLeg(-80, BLTarget, BLRaycastOrigin);
+
+
+        if (Vector3.Distance(BLRaycastOrigin.position, BLTarget.position) >= _maxDistance && !DOTween.IsTweening(BLTarget, true))
+            PlaceBackLeg(-60, BLTarget, BLRaycastOrigin);
+
+        if (Vector3.Distance(BRRaycastOrigin.position, BRTarget.position) >= _maxDistance && !DOTween.IsTweening(BRTarget, true))
+            PlaceBackLeg(60, BRTarget, BRRaycastOrigin);
+
+
+
+        if (Vector3.Distance(FLRaycastOrigin.position, FLTarget.position) <= _flPreviousDistance && !DOTween.IsTweening(FLTarget, true))
+            _flPreviousDistance = Vector3.Distance(FLRaycastOrigin.position, FLTarget.position);
+        else if (!DOTween.IsTweening(FLTarget, true))
             PlaceLeg(1, FLTarget, FLRaycastOrigin);
+        else
+            _flPreviousDistance = _maxDistance;
+
+        if (Vector3.Distance(FRRaycastOrigin.position, FRTarget.position) <= _frPreviousDistance && !DOTween.IsTweening(FRTarget, true))
+            _frPreviousDistance = Vector3.Distance(FRRaycastOrigin.position, FRTarget.position);
+        else if (!DOTween.IsTweening(FRTarget, true))
+            PlaceLeg(-1, FRTarget, FRRaycastOrigin);
+        else
+            _frPreviousDistance = _maxDistance;
     }
     
     public void Init()
     {
-        PlaceLeg(-1, BRTarget, BRRaycastOrigin);
-        PlaceLeg(1, BLTarget, BLRaycastOrigin);
-        PlaceLeg(-1, FRTarget, FRRaycastOrigin);
+        PlaceBackLeg(-60, BLTarget, BLRaycastOrigin);
+        PlaceBackLeg(60, BRTarget, BRRaycastOrigin);
         PlaceLeg(1, FLTarget, FLRaycastOrigin);
+        PlaceLeg(-1, FRTarget, FRRaycastOrigin);
     }
 
-    void PlaceLeg(int rotationDirection, Transform legTarget, Transform RaycastOrigin)
+    void PlaceLeg(int rotationIncrement, Transform legTarget, Transform RaycastOrigin)
     {
-        for (int i = 0; i != 90 * rotationDirection; i = i - 5 * rotationDirection)
+        for (int i = 0; ; i = i - 5 * rotationIncrement)
         {
             _direction = Quaternion.AngleAxis(i, Vector3.back) * Bug.transform.right;
             Debug.DrawRay(RaycastOrigin.position, _direction * _maxDistance, Color.blue, 10.0f);
@@ -62,29 +85,30 @@ public class BugController : MonoBehaviour
         Vector3 halfwayPoint = ((_hit.point - legTarget.position) * 0.5f) + legTarget.position;
         Vector3 halfwayPointDirection = _hit.point - legTarget.position;
 
-        Vector3 direction = Quaternion.AngleAxis(90 * rotationDirection, Vector3.back) * halfwayPointDirection;
+        Vector3 direction = Quaternion.AngleAxis(90 * rotationIncrement, Vector3.back) * halfwayPointDirection;
         halfwayPoint = halfwayPoint + (direction * 0.25f);
         Vector3[] path = new Vector3[] { legTarget.position, halfwayPoint, _hit.point };
 
         legTarget.DOPath(path, 0.25f, PathType.CatmullRom, PathMode.Full3D, 5, Color.green).SetEase(Ease.Linear);
         legTarget.DORotateQuaternion(Quaternion.LookRotation(_hit.normal), 0.5f);
+    }
 
+    void PlaceBackLeg(int rotationDirection, Transform legTarget, Transform RaycastOrigin)
+    {
+        _direction = Quaternion.AngleAxis(rotationDirection, Vector3.back) * Bug.transform.right;
+        Debug.DrawRay(RaycastOrigin.position, _direction * _maxDistance, Color.blue, 10.0f);
 
-        //    _direction = Quaternion.AngleAxis(angle, Vector3.back) * Bug.transform.right;
-        //Debug.DrawRay(RaycastOrigin.position, _direction * 1000.0f, Color.blue, 10.0f);
+        if (Physics.Raycast(RaycastOrigin.position, _direction, out _hit, _maxDistance, 1 << LayerMask.NameToLayer("Wall")))
+        {
+            Vector3 halfwayPoint = ((_hit.point - legTarget.position) * 0.5f) + legTarget.position;
+            Vector3 halfwayPointDirection = _hit.point - legTarget.position;
 
+            Vector3 direction = Quaternion.AngleAxis(90 * rotationDirection, Vector3.back) * halfwayPointDirection;
+            halfwayPoint = halfwayPoint + (direction * 0.25f);
+            Vector3[] path = new Vector3[] { legTarget.position, halfwayPoint, _hit.point };
 
-        //if (Physics.Raycast(RaycastOrigin.position, _direction, out _hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Wall")))
-        //{
-        //    Vector3 halfwayPoint = ((_hit.point - legTarget.position) * 0.5f) + legTarget.position;
-        //    Vector3 halfwayPointDirection = _hit.point - legTarget.position;
-
-        //    Vector3 direction = Quaternion.AngleAxis(90 * angleSign, Vector3.back) * halfwayPointDirection;
-        //    halfwayPoint = halfwayPoint + (direction * 0.25f);
-        //    Vector3[] path = new Vector3[] { legTarget.position, halfwayPoint, _hit.point };
-
-        //    legTarget.DOPath(path, 0.25f, PathType.CatmullRom, PathMode.Full3D, 5, Color.green).SetEase(Ease.Linear);
-        //    legTarget.DORotateQuaternion(Quaternion.LookRotation(_hit.normal), 0.5f);
-        //}
+            legTarget.DOPath(path, 0.25f, PathType.CatmullRom, PathMode.Full3D, 5, Color.green).SetEase(Ease.Linear);
+            legTarget.DORotateQuaternion(Quaternion.LookRotation(_hit.normal), 0.5f);
+        }
     }
 }
