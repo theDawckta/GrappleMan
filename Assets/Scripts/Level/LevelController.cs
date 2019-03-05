bf91b14584f2a315cc27beb4cb5e774b0129d186
Zeke Lasater <zekelasater@Zekes-MacBook-Pro.local>
2019-03-04 23:36:15 -0800

    small adjustments

--- Assets/Scripts/Level/LevelController.cs
+++ Assets/Scripts/Level/LevelController.cs
@@ -59,7 +59,7 @@ public class LevelController : MonoBehaviour
     public void StartDeathMarch()
     {
         _pressureTween = Pressure.transform.DOPath(_pressureWayoints.ToArray(), 5.0f, PathType.CatmullRom, PathMode.Full3D, 10, Color.green).SetSpeedBased().OnWaypointChange(PressureWaypointChange).SetLookAt(0.001f, Vector3.left);
-        _pressureTween.timeScale = 2.0f;
+        _pressureTween.timeScale = 1.0f;
         _startPlatform.gameObject.SetActive(false);
     }
 
